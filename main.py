905186aab1884c6d6121dc9345a4541ed99693a6
Łukasz Pełka <lukasz.pelka.ziip@gmail.com>
2023-01-16 11:39:17 +0100

    Add css and bootstrap in project

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import dash
 from dash import Dash, html, dcc
 from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
 
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = html.Div([
 
